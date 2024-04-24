7e1ca8105b4fe29fe39c4ce6224bb23fc3332f04
GitAjay <68543757+GitAjay123@users.noreply.github.com>
2024-04-24 15:55:40 +0530

    product.js

--- src/product.js
+++ src/product.js
@@ -1,181 +1,173 @@
 import fixText from "./fixtext";
 
 const product = async (query) => {
-    try {
-        const product_page = await (
+         const product_page = await (
             await fetch(`https://www.amazon.in/${query}`)
-        ).text();
+         ).text();
 
-        var features = [];
-        var feat = product_page
-            .split('<ul class="a-unordered-list a-vertical a-spacing-mini">')[1]
-            .split("</ul>")[0];
-        var feat = feat.split('<span class="a-list-item">');
-        for (var i = 1; i < feat.length; i++) {
-            try {
-                features.push(fixText(feat[i].split("</span>")[0]));
-            } catch (err) {}
-        }
+         try {
+            var features = [];
+            var feat = product_page
+               .split('<ul class="a-unordered-list a-vertical a-spacing-mini">')[1]
+               .split("</ul>")[0];
+            var feat = feat.split('<span class="a-list-item">');
+            for (var i = 1; i < feat.length; i++) {
+               try {
+                  features.push(fixText(feat[i].split("</span>")[0]));
+               } catch (err) {}
+            }
+         } catch (err) {
+            var features = [null];
+         }
 
-        var price = null;
-        var original_price = null;
+         var price = null;
+         var original_price = null;
 
-        try {
+         try {
             var pricediv = product_page.split(/<div id="unifiedPrice_feature_div".*>/g);
 
             original_price = pricediv[1]
-                .split('<span class="a-offscreen">')[1]
-                .split("</span>")[0];
+               .split('<span class="a-offscreen">')[1]
+               .split("</span>")[0];
 
             try {
-                price = pricediv[1]
-                    .split(
-                        '<span class="a-price a-text-price a-size-medium apexPriceToPay" data-a-size="b" data-a-color="price">'
-                    )[1]
-                    .split("</span>")[0];
-                if (price.includes(">")) {
-                    price = price.split(">")[1];
-                }
+               price = pricediv[1]
+                  .split(
+                     '<span class="a-price a-text-price a-size-medium apexPriceToPay" data-a-size="b" data-a-color="price">'
+                  )[1]
+                  .split("</span>")[0];
+               if (price.includes(">")) {
+                  price = price.split(">")[1];
+               }
             } catch (pe) {}
 
             if (price === null) {
-                price = pricediv[1]
-                    .split(/<span class="a-price-whole">/g)[1]
-                    .split("</span>")[0];
+               price = pricediv[1]
+                  .split(/<span class="a-price-whole">/g)[1]
+                  .split("</span>")[0];
             }
 
             // Additional price extraction logic
             if (!price) {
-                // Check the additional span element for price
-                const additionalPriceElement = product_page
-                    .split('<span class="a-price aok-align-center"')[1]
-                    .split('</span>')[0];
-                price = additionalPriceElement.trim();
+               // Check the additional span element for price
+               const additionalPriceElement = product_page
+                  .split('<span class="a-price aok-align-center"')[1]
+                  .split('</span>')[0];
+               price = additionalPriceElement.trim();
             }
 
             // Additional span id price extraction
             if (!price) {
-                const priceElement = product_page
-                    .split('<span id="priceblock_ourprice"')[1]
-                    .split('</span>')[0];
-                price = priceElement.trim();
+               const priceElement = product_page
+                  .split('<span id="priceblock_ourprice"')[1]
+                  .split('</span>')[0];
+               price = priceElement.trim();
             }
+
 
             // Additional price extraction logic
             if (!price) {
-                const priceElement = product_page
-                    .split('<span class="a-price-symbol">₹</span>')[1]
-                    .split('<span class="a-price-whole">')[1]
-                    .split("</span>")[0];
-                price = priceElement.trim();
+               const priceElement = product_page
+                  .split('<span class="a-price-symbol">₹</span>')[1]
+                  .split('<span class="a-price-whole">')[1]
+                  .split("</span>")[0];
+               price = priceElement.trim();
             }
-        } catch (error) {}
+         } catch (error) {}
 
-        if (original_price !== null) {
+
+         if (original_price !== null) {
             original_price = parseFloat(
-                original_price.replace("₹", "").replace(/,/g, "").trim()
+               original_price.replace("₹", "").replace(/,/g, "").trim()
             );
-        }
-        if (price !== null) {
+         }
+         if (price !== null) {
             price = parseFloat(price.replace("₹", "").replace(/,/g, "").trim());
-        }
+         }
 
-        var in_stock = false;
-        try {
-            in_stock = product_page
-                .split('id="availability"')[1]
-                .split("</div>")[0]
-                .toLowerCase()
-                .lastIndexOf("in stock.") !== -1;
-        } catch (e) {
-            in_stock = product_page.split("In stock.").length > 1;
-        }
+         try {
+            var in_stock =
+               product_page
+               .split('id="availability"')[1]
+               .split("</div>")[0]
+               .toLowerCase()
+               .lastIndexOf("in stock.") !== -1;
+         } catch (e) {
+            var in_stock = product_page.split("In stock.").length > 1;
+         }
 
-        var image = null;
-        try {
-            image = product_page
-                .split('<div id="imgTagWrapperId" class="imgTagWrapper">')[1]
-                .split('data-old-hires="')[1]
-                .split('"')[0]
-                .replaceAll("\n", "");
+         try {
+            var image = product_page
+               .split('<div id="imgTagWrapperId" class="imgTagWrapper">')[1]
+               .split('data-old-hires="')[1]
+               .split('"')[0]
+               .replaceAll("\n", "");
             if (image === "") {
-                image = product_page
-                    .split('<div id="imgTagWrapperId" class="imgTagWrapper">')[1]
-                    .split('data-a-dynamic-image="{&quot;')[1]
-                    .split("&quot;")[0]
-                    .replaceAll("\n", "");
+               var image = product_page
+                  .split('<div id="imgTagWrapperId" class="imgTagWrapper">')[1]
+                  .split('data-a-dynamic-image="{"')[1]
+                  .split(""
+                     ")[0]
+                     .replaceAll("\n", "");
+                  }
+            } catch (e) {
+               var image = null;
             }
-        } catch (e) {}
 
-        var ratings_count = null;
-        var rating = null;
-        var rating_details = null;
-        try {
-            var review_section = product_page.split("ratings</span>")[0];
-            ratings_count = parseInt(
-                lastEntry(review_section.split(">")).replace(/,/g, "").trim()
+            try {
+               var review_section = product_page.split("ratings</span>")[0];
+               var ratings_count = parseInt(
+                  lastEntry(review_section.split(">")).replace(/,/g, "").trim()
+               );
+               var rating = parseFloat(
+                  lastEntry(
+                     lastEntry(review_section.split("a-icon-star"))
+                     .split("</span>")[0]
+                     .split("out of")[0]
+                     .split(">")
+                  ).trim()
+               );
+               var rating_details = {
+                  ratings_count,
+                  rating
+               };
+            } catch (er) {
+               console.log(er.message);
+               var rating_details = null;
+            }
+
+            try {
+               var product_detail = {
+                  name: fixText(
+                     product_page
+                     .split(
+                        '<span id="productTitle" class="a-size-large product-title-word-break">'
+                     )[1]
+                     .split("</span>")[0]
+                  ),
+                  image,
+                  price,
+                  original_price,
+                  in_stock,
+                  rating_details,
+                  features,
+                  product_link: `https://www.amazon.in/${query}`,
+               };
+            } catch (err) {
+               var product_detail = null;
+            }
+
+            return JSON.stringify({
+                  status: true,
+                  query,
+                  fetch_from: `https://www.amazon.in/${query}`,
+                  product_detail,
+               },
+               null,
+               2
             );
-            rating = parseFloat(
-                lastEntry(
-                    lastEntry(review_section.split("a-icon-star"))
-                    .split("</span>")[0]
-                    .split("out of")[0]
-                    .split(">")
-                ).trim()
-            );
-            rating_details = {
-                ratings_count,
-                rating
-            };
-        } catch (er) {
-            console.log(er.message);
-        }
+         };
 
-        var name = null;
-        try {
-            name = fixText(
-                product_page
-                .split('<span id="productTitle" class="a-size-large product-title-word-break">')[1]
-                .split("</span>")[0]
-            );
-        } catch (err) {}
+         const lastEntry = (array) => array[array.length - 1];
 
-        var product_link = `https://www.amazon.in/${query}`;
-
-        var product_detail = null;
-        if (name !== null) {
-            product_detail = {
-                name,
-                image,
-                price,
-                original_price,
-                in_stock,
-                rating_details,
-                features,
-                product_link,
-            };
-        }
-
-        return JSON.stringify({
-                status: true,
-                query,
-                fetch_from: `https://www.amazon.in/${query}`,
-                product_detail,
-            },
-            null,
-            2
-        );
-    } catch (err) {
-        console.error("Error fetching product details:", err);
-        return JSON.stringify({
-            status: false,
-            error: "Error fetching product details",
-            query,
-            fetch_from: `https://www.amazon.in/${query}`,
-        });
-    }
-};
-
-const lastEntry = (array) => array[array.length - 1];
-
-export default product;
+         export default product;
